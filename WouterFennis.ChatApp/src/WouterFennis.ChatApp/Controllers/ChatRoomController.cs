fdd0463a40d55553d528d9b3cff487040293d366
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-14 20:58:56 +0100

    ADDED working Endpoint for searching for specific chatroom

--- WouterFennis.ChatApp/src/WouterFennis.ChatApp/Controllers/ChatRoomController.cs
+++ WouterFennis.ChatApp/src/WouterFennis.ChatApp/Controllers/ChatRoomController.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WouterFennis.ChatApp.Domain;
 using WouterFennis.ChatApp.DAL.Repositories;
+using System.Net;
+using Swashbuckle.SwaggerGen.Annotations;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,13 +21,37 @@ namespace WouterFennis.ChatApp.Controllers
         }
         // GET: api/v1/ChatRoom
         [HttpGet]
+        [SwaggerOperation("GetAllChatRooms")]
+        [ProducesResponseType(typeof(IEnumerable<ChatRoom>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequestResult), (int)HttpStatusCode.BadRequest)]
         public IEnumerable<ChatRoom> Get()
-        {           
-            return _chatRoomRepository.FindAll();
+        {       
+            IEnumerable<ChatRoom> chatRooms = _chatRoomRepository.FindAll();
+            return chatRooms;
+        }
+
+        // GET: api/v1/ChatRoom/1
+        [HttpGet]
+        [Route("{id}")]
+        [SwaggerOperation("GetChatRoomById")]
+        [ProducesResponseType(typeof(ChatRoom), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(NotFoundResult), (int)HttpStatusCode.NotFound)]
+        public IActionResult GetById(long id)
+        {
+            ChatRoom foundChatRoom = null;
+            if (_chatRoomRepository.Exists(id))
+            {
+                foundChatRoom = _chatRoomRepository.FindByIdWithMessages(id).FirstOrDefault();
+                return new ObjectResult(foundChatRoom);
+            }
+            return NotFound();
         }
 
         // POST api/v1/ChatRoom
         [HttpPost]
+        [SwaggerOperation("AddChatRoom")]
+        [ProducesResponseType(typeof(OkResult), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequestResult), (int)HttpStatusCode.BadRequest)]
         public ActionResult Post([FromBody]ChatRoom chatRoom)
         {
             if (!ModelState.IsValid)

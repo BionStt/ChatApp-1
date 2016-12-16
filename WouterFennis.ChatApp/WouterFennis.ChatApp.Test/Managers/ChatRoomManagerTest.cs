182c696b55bc4fb0bb75ca223faf2b1e911125a2
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-16 20:39:46 +0100

    ADDED chatroom manager

---
+++ WouterFennis.ChatApp/WouterFennis.ChatApp.Test/Managers/ChatRoomManagerTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WouterFennis.ChatApp.Test.Managers
+{
+    public class ChatRoomManagerTest
+    {
+        //var chatRoomRepositoryMock = new Mock<IRepository<ChatRoom, long>>(MockBehavior.Strict);
+        //chatRoomRepositoryMock.Setup(repository => repository.FindAll()).Returns(new List<ChatRoom>());
+    }
+}
---
+++ WouterFennis.ChatApp/src/WouterFennis.ChatApp/Managers/IChatRoomManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WouterFennis.ChatApp.Domain;
+
+namespace WouterFennis.ChatApp.Managers
+{
+    public interface IChatRoomManager
+    {
+        IEnumerable<ChatRoom> GetAllChatRooms();
+        ChatRoom FindChatRoomById(long id);
+        void AddMessageToChatRoom(long chatRoomId, Message message);
+        long AddChatRoom(ChatRoom chatRoom);
+    }
+}

a6de8377a19d00105d670581c2bee660589b5702
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-18 15:57:10 +0100

    ADDED overview controller content

--- WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/Controllers/ChatRoomOverviewController.cs
+++ WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/Controllers/ChatRoomOverviewController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WouterFennis.ChatApp.FrontEnd.ViewModels;
+using WouterFennis.ChatApp.FrontEnd.Agents;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,21 @@ namespace WouterFennis.ChatApp.FrontEnd.Controllers
 {
     public class ChatRoomOverviewController : Controller
     {
+        private readonly IChatRoomBackendServiceAgent _chatRoomService;
+
+        public ChatRoomOverviewController(IChatRoomBackendServiceAgent chatRoomService)
+        {
+            _chatRoomService = chatRoomService;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<ChatRoomViewModel> chatRoomList = new List<ChatRoomViewModel>();
+
+            var chatRoomList2 = _chatRoomService.GetAllChatRooms();
+
+            return View(chatRoomList);
         }
     }
 }

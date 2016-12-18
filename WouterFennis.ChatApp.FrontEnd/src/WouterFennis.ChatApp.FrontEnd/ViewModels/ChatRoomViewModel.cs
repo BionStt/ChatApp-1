28745ca1e3b8a95d3e2b3ef0dc5260ccc2a2ff54
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-18 15:58:58 +0100

    ADDED chatroom viewmodel

---
+++ WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/ChatRoomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WouterFennis.ChatApp.FrontEnd.ViewModels
+{
+    public class ChatRoomViewModel
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}

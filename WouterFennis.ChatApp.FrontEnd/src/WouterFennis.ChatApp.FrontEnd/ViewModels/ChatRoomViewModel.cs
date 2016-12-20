ae9a4f223a48aa1eff7071d20591ca7c4549ccaa
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-20 23:02:36 +0100

    CHANGED viewmodel properties

--- WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/ChatRoomViewModel.cs
+++ WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/ChatRoomViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace WouterFennis.ChatApp.FrontEnd.ViewModels
 {
     public class ChatRoomViewModel
     {
-        public long Id { get; set; }
+        public long? Id { get; set; }
 
+        [Display(Name = "Naam")]
+        [Required]
         public string Name { get; set; }
     }
 }

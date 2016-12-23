c7ee0c2c2be1ef6a9249066cb15eb46b99e04337
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-23 20:39:32 +0100

    ADDED MessageViewModel

---
+++ WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/MessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WouterFennis.ChatApp.FrontEnd.ViewModels
+{
+    public class MessageViewModel
+    {
+        public long? Id { get; set; }
+
+        [Display(Name = "Bericht")]
+        [Required]
+        public string Content { get; set; }
+
+        [Display(Name = "Verzender")]
+        [Required]
+        public string Sender { get; set; }
+
+        [Display(Name = "Gemaakt op")]
+        public DateTime? CreatedOnUtc { get; set; }
+    }
+}

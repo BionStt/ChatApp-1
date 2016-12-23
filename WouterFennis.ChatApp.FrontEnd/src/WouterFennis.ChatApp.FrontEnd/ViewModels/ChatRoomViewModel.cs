536404256ae217a483c02c838b77c17dfb95cdc0
Wouter Fennis <wouter_hilversum@hotmail.com>
2016-12-23 20:37:05 +0100

    ADDED more logic to the chatroom viewmodel

--- WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/ChatRoomViewModel.cs
+++ WouterFennis.ChatApp.FrontEnd/src/WouterFennis.ChatApp.FrontEnd/ViewModels/ChatRoomViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WouterFennis.ChatApp.FrontEnd.Agents.Models;
 
 namespace WouterFennis.ChatApp.FrontEnd.ViewModels
 {
@@ -13,5 +14,31 @@ namespace WouterFennis.ChatApp.FrontEnd.ViewModels
         [Display(Name = "Naam")]
         [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Berichten")]
+        public List<MessageViewModel> Messages;
+
+        public ChatRoomViewModel() { }
+
+        public ChatRoomViewModel(ChatRoom chatRoom)
+        {
+            Id = chatRoom.Id;
+            Name = chatRoom.Name;
+            Messages = convertAgentMessagesToViewModel(chatRoom.Messages);
+        }
+
+        private List<MessageViewModel> convertAgentMessagesToViewModel(IEnumerable<Message> messages)
+        {
+            var query = from message in messages
+                        select new MessageViewModel()
+                        {
+                            Id = message.Id,
+                            Content = message.Content,
+                            Sender = message.Sender,
+                            CreatedOnUtc = message.CreatedOnUtc
+                        };
+            List<MessageViewModel> viewModelMessages = query.ToList();
+            return viewModelMessages;
+        }
     }
 }
